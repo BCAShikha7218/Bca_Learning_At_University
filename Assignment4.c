cc63fc98447d1b7297ff80ffd7fa0705cef79aab
BCAShikha7218 <127484852+BCAShikha7218@users.noreply.github.com>
2023-03-18 00:56:37 +0530

    Update Assignment4.c

--- Assignment4.c
+++ Assignment4.c
@@ -2,6 +2,47 @@
 
 int main()
 {
-  
-  return 0;
+  #include <stdio.h>
+#include <stdlib.h> 
+ 
+int main()
+{
+    int n,i, j,**a ,max;
+    printf("\nEnter the N value for square matrix :");
+    scanf("%d",&n); 
+     
+    a= (int **)calloc(n,sizeof(int*));
+    for (i=0; i<n; i++) 
+         a[i] =(int *)calloc(n,sizeof(int*));
+    printf("\nEnter the elements in %dX%d matrix :",n,n);
+    for (i = 0; i<n;i++)
+    {
+    	for(j=0;j<n; j++)
+        {
+		 scanf("%d",&a[i][j]);
+	    }	
+	} 
+    printf("\ngiven  matrix :\n");
+ 
+    for (i = 0; i < n;i++)
+	{
+        for (j = 0; j < n; j++)
+        {
+          printf("%d\t", a[i][j]);	
+		}    
+        printf("\n");
+    }
+    for (i=0;i<n;i++)
+    {
+    	max=a[i][0];
+    	for(j=1;j<n;j++)
+    	{
+    		if(a[i][j]>max)
+    		{
+    		 max=a[i][j];
+		    }
+        }
+        printf("max in row %d  is %d\n",i+1,max);
+	} 
+    return 0;
 }
