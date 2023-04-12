7641b09973108941b4492ec9476370c202294606
BCAShikha7218 <127484852+BCAShikha7218@users.noreply.github.com>
2023-04-13 00:23:10 +0530

    Update Assignment7.c
    ok

--- Assignment7.c
+++ Assignment7.c
@@ -2,5 +2,28 @@
 
 int main()
 {
+  #include <stdio.h>
+#include <string.h>
+  
+int main()
+{
+    char str[1000],rev[1000];
+    int i,j,count=0;
+  
+    printf("Enter the String: ");
+    scanf("%s", str);
+  
+    for (; str[count]!='\0'; ++count);
+  
+    printf("\nLength of Str is %d", count);
+ 	j=count-1;
+ 	for(i=0; i<count; i++)
+ 	{
+ 		rev[i]= str[j];
+ 		j--;
+	 }
+  	printf("\n The reverse of the string is: %s",rev);
+    return 0;
+}
   return 0;
 }
