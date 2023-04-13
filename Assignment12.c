139c405cda36793f6db1fb854b07e5fc674e3dc4
Shikha <127484852+BCAShikha7218@users.noreply.github.com>
2023-04-13 13:42:24 +0530

    Update Assignment12.c

--- Assignment12.c
+++ Assignment12.c
@@ -2,5 +2,18 @@
 
 int main()
 {
+           float mark[10] = {45.6, 78.4, 65.9, 58.3, 82.1, 44.5, 61.8, 53.6, 49.2, 37.7}; 
+         int i; 
+         float sum = 0, highest = 0; 
+         
+         for (i = 0; i < 10; i++) 
+              { 
+                 sum += mark[i]; 
+                 if (mark[i] > highest) 
+                         highest = mark[i]; 
+              } 
+       
+         printf("The Highest Mark is %5.2f \n", highest); 
+         getch(); 
   return 0;
 }
