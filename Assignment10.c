8a0ebb2065cc80ce997d737cf08d1a72c3ab28bf
Shikha <127484852+BCAShikha7218@users.noreply.github.com>
2023-04-15 00:08:49 +0530

    Update Assignment10.c

--- Assignment10.c
+++ Assignment10.c
@@ -2,17 +2,11 @@
 //If all elements in the array are odd it is known as "ODD ARRAY"
 //if all elements in the array are even it is known as "EVEN ARRAY"
 //if both even and odd available it is known as MIXED array
-
-
-int findType(int n, int *) //complete this function
+#include<stdio.h>
+int findType(int n, int *arr)
 {
-       int arr[n];
    int i;
    int odd = 0, even = 0;
-   printf("enter the elements into an array:");
-   for(i = 0; i < n; i++){
-      scanf("%d",&arr[i]);
-   }
    for(i = 0; i < n; i++){
       if(arr[i] % 2 == 1)
          odd++;
@@ -23,13 +17,18 @@ int findType(int n, int *) //complete this function
    else if(even == n)
       printf("Even Array");
    else
-      printf("Mixed Array");   
+      printf("Mixed Array");
+    
 }
 int main()
 {
-   int n;
-   printf("enter no of elements:");
-   scanf("%d",&n);
-   findType(n);
+	int n,arr[n],i,a;
+    printf("enter no of elements:");
+    scanf("%d",&n);
+     printf("enter the elements into an array:");
+   for(i = 0; i < n; i++){
+      scanf("%d",&arr[i]);
+  }
+  findType(n,arr);
   return 0;
 }
